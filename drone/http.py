0fd98e059360a09c28aefa9e94eed972914a781e
Harish Navnit <harishnavnit@gmail.com>
2021-01-11 11:41:36 +0530

    Fix request semantics
    https://github.com/tinvaan/PyDroneio/issues/4

--- drone/http.py
+++ drone/http.py
@@ -9,8 +9,7 @@ from .decorators import jsonargs
 @jsonargs
 def get(url, params=None):
     headers = {'Authorization': 'Bearer %s' % config.api_token()}
-    r = requests.get(url, headers=headers) if not params else \
-        requests.get(url, headers=headers, params=params)
+    r = requests.get(url, headers=headers, params=params)
     if not r.ok:
         r.raise_for_status()
     return r.json()
@@ -19,14 +18,7 @@ def get(url, params=None):
 @jsonargs
 def post(url, params=None, data=None):
     headers = {'Authorization': 'Bearer %s' % config.api_token()}
-    if params and data:
-        r = requests.post(url, headers=headers, params=params, data=data)
-    elif params:
-        r = requests.post(url, headers=headers, params=params)
-    elif data:
-        r = requests.post(url, headers=headers, data=data)
-    else:
-        r = requests.post(url, headers=headers)
+    r = requests.post(url, headers=headers, params=params, data=data)
     if not r.ok:
         r.raise_for_status()
     return r.json()
@@ -35,14 +27,7 @@ def post(url, params=None, data=None):
 @jsonargs
 def patch(url, params=None, data=None):
     headers = {'Authorization': 'Bearer %s' % config.api_token()}
-    if params and data:
-        r = requests.patch(url, headers=headers, params=params, data=data)
-    elif params:
-        r = requests.patch(url, headers=headers, params=params)
-    elif data:
-        r = requests.patch(url, headers=headers, data=data)
-    else:
-        r = requests.patch(url, headers=headers)
+    r = requests.patch(url, headers=headers, params=params, data=data)
     if not r.ok:
         r.raise_for_status()
     return r.json()
